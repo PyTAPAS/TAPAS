3ee37306c6522f65f6c40defd867f7f05cbbcd02
philippfrech <philipp-frech@tutanota.de>
2025-05-21 16:02:36 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,3 +8,11 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
+
+html_theme_options = {
+    "repository_url": "https://github.com/pytapas/tapas",
+    "repository_branch": "main",
+    "path_to_docs": "docs",
+    "use_repository_button": True,
+    "use_issues_button": True,
+}
