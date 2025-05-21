3009e9b53e3efbbf08bd65771bfc5f20048fa93f
philippfrech <philipp-frech@tutanota.de>
2025-05-21 14:56:01 +0200

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project   = "TAPAS"
+author    = "Philipp Frech"
+release   = "1.0.2"
+
+extensions = []
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
