631951781d95cb34e8cb51821072601a8ba35223
philippfrech <philipp-frech@tutanota.de>
2025-05-22 20:59:31 +0200

    Update pyinstaller.py

--- build/pyinstaller.py
+++ build/pyinstaller.py
@@ -135,8 +135,10 @@ pyinstaller_args = [
     '--hidden-import', 'pyqtconfig',
     '--hidden-import', 'h5py',
     '--hidden-import', 'jax.experimental.ode',
+    '--hidden-import', 'jax._src.lib.utils',
 
     '--collect-submodules', 'jax',
+    '--collect-binaries',  'jaxlib',
     '--collect-binaries',  'jaxlib',
     
 
